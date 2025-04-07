876a0a8056ad30f37f8bdd69078dbb19cba20b34
Mohammad Rabiul Hasan <116081842+Rabiul35790@users.noreply.github.com>
2025-04-07 18:31:28 +0600

    Update factorial_iterative_dsa.java

--- factorial_iterative_dsa.java
+++ factorial_iterative_dsa.java
@@ -10,10 +10,10 @@ public class Main
         return res;
     }
     
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-        int res = fac(n);
-        System.out.println(res);
-	}
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int n = sc.nextInt();
+	int res = fac(n);
+	System.out.println(res);
+     }
 }
