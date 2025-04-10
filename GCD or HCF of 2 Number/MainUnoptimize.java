dc78531d0a4f30ca7916bf256892dde54e17fd82
Rabiul35790 <roniahmed35790@gmail.com>
2025-04-10 15:18:22 +0600

    GCD Unoptimize

---
+++ GCD or HCF of 2 Number/MainUnoptimize.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class MainUnoptimize {
+
+    static int gcdhcf (int a, int b) {
+        int min = 0;
+        if(a<b) {
+            min = a;
+        }else{
+            min = b;
+        }
+        for(int i=min; i >= 1; i--) {
+            if(a % i == 0 && b % i == 0) {
+                return i;
+            }
+        }
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(gcdhcf(a, b));
+        sc.close();
+    }
+    
+}
