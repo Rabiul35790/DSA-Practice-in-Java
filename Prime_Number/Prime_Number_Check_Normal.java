e064520947642886aa85586919b2f447d493ca8d
Rabiul35790 <roniahmed35790@gmail.com>
2025-04-13 16:23:59 +0600

    n/2 solution done

---
+++ Prime_Number/Prime_Number_Check_Normal.java
@@ -0,0 +1,23 @@
+
+
+package Prime_Number;
+import java.util.Scanner;
+
+public class Prime_Number_Check_Normal {
+    static boolean isPrime(int a){
+        for(int i = 2; i <= a/2; i++)
+        {
+            if(a % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(isPrime(a));
+        sc.close();
+    }
+}
