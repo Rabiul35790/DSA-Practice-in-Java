8516a5728d17b2862496cfe8009e6787f3676a1c
Rabiul35790 <roniahmed35790@gmail.com>
2025-04-17 17:59:33 +0600

    root n done

---
+++ Print_all_divisorof_number/Root_n_Solution.java
@@ -0,0 +1,25 @@
+package Print_all_divisorof_number;
+
+import java.util.Scanner;
+
+public class Root_n_Solution {
+
+    static void printDivisor(int a) {
+        for(int i = 1; i*i <= a; i++) {
+            if(a%i == 0) {
+                System.out.println(i);
+                System.out.println(a/i);
+            }
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        printDivisor(a);
+        sc.close();
+    }
+    
+}
