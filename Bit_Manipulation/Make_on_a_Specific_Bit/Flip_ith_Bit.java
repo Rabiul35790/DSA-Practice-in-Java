5dac001c3a1b2ce9206b2a9c521ed7d23c208eb5
Rabiul35790 <roniahmed35790@gmail.com>
2025-05-04 14:56:53 +0600

    flip ith position bit done

---
+++ Bit_Manipulation/Make_on_a_Specific_Bit/Flip_ith_Bit.java
@@ -0,0 +1,27 @@
+package Bit_Manipulation.Make_on_a_Specific_Bit;
+
+import java.util.Scanner;
+
+public class Flip_ith_Bit {
+
+    static int makeMask(int i) {
+        int mask = 1 << i;
+        return mask;
+    }
+
+    static int flipBit(int n, int i) {
+        int mask = makeMask(i);
+        return n | mask;
+    }
+    
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = sc.nextInt();
+        int res = flipBit(n,i);
+        System.out.println(res);
+        sc.close(); 
+    }
+}
