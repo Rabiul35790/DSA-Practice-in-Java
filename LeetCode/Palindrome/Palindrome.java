a3710a192bba6db89c512f9d6f9df20a59ceb5ea
Rabiul35790 <roniahmed35790@gmail.com>
2025-05-06 14:23:54 +0600

    Problem 9 palindrome added without converting into string

---
+++ LeetCode/Palindrome/Palindrome.java
@@ -0,0 +1,59 @@
+//Without converting to string solution
+import java.util.Scanner;
+class Palindrome {
+
+    public int[] buildArray(int[] nums) {
+        int n = nums.length;
+        int [] ans = new int[n];
+        for(int i = 0; i < nums.length; i++)
+        {
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+        
+    }
+
+    static int countPow(int x) {
+        int count = 0;
+        int pow = 1;
+        while(x>0) {
+            x = x / 10;
+            count++;
+        }
+        for(int i = count; i>1; i--) {
+            pow = pow * 10;
+        }
+        return pow;
+    }
+    static boolean isPalindrome(int x) {
+        if (x == 0) {
+           return true; 
+        }
+        if(x > 0) {
+        int temp = x;
+        int p = countPow(x);
+        int rev = 0;
+        while(x>0) {
+            int r = x % 10;
+            x = x / 10;
+            rev = rev + (r * p); 
+            p = p / 10;
+        }
+        if(rev == temp)
+        {
+            return true;
+        } else{
+            return false;
+        } 
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int x = sc.nextInt();
+            System.out.println(isPalindrome(x));
+            sc.close();
+        }
+}
