b5c1043b1db1bd0628e1409726f97b1f1c07f142
Rabiul35790 <roniahmed35790@gmail.com>
2025-05-08 18:00:37 +0600

    RMSB non optimal solution done

---
+++ Bit_Manipulation/Position_of_Right_Most_Set_Bit/Pos_of_RMSB.java
@@ -0,0 +1,34 @@
+package Bit_Manipulation.Position_of_Right_Most_Set_Bit;
+
+import java.util.Scanner;
+
+public class Pos_of_RMSB {
+
+    static int posOfRMSB(int n) {
+
+        if(n == 0) {
+            return 0;
+        }
+        int mask = 1;
+        int pos = 1;
+        while((n & mask) == 0) {
+            mask = mask << 1;
+            pos++;
+        }
+        return pos;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int pos = posOfRMSB(n);
+        if(pos == 0) {
+            System.out.println("No set bit present in the number");
+        }else {
+            System.out.println(pos);
+        }
+        sc.close();
+    }
+    
+}
